ed0691637fff55e84bb6953f0918276d7801c1b1
Samir Hasanov <samirhsn0104@gmail.com>
2023-07-13 14:24:21 +0400

    project created

---
+++ app.js
@@ -0,0 +1,64 @@
+// let person = prompt("ad daxil edin:");
+// let onePart = person.slice(0, 1);
+// let twoPart = person.slice(1);
+
+// onePart = onePart.toUpperCase();
+
+// console.log(onePart + twoPart);
+
+// let result = person.replace(person[0], person[0].toUpperCase())
+
+// console.log(result)
+
+// 10-100 arasinda 3 e bolunen ededler
+// for (let i = 10; i < 100; i++) {
+//     if(i%3==0){
+//         console.log(i);
+//     }
+// }
+
+// let v=1;
+// let f1 = function(){
+//     console.log(v);
+// };
+
+// let f2 = function() {
+//     v=2;
+//     f1();
+// };
+
+// f2();
+
+// const x = 6%2;
+// const y=x?'one':'two'
+
+// console.log(y)
+
+// let a =5;
+// console.log(++a,a++)
+
+// let animal = ['jaguar', 'eagle'];
+
+// console.log(animal.pop())
+
+// console.log(typeof 'true')
+
+// const myNumber = [1,2,3,4,5,6,7];
+// const myFunction = (arr) => {
+//     return arr.map((x) => x+3).filter((x) => x < 7);
+
+// };
+// console.log(myFunction(myNumber))
+
+// let rainForest =10;
+// let animal = 0;
+
+// while(rainForest<13||animal<=2){
+//     rainForest++;
+//     animal+=2;
+// }
+
+// console.log(animal)
+
+//  console.log(NaN) 
+     
